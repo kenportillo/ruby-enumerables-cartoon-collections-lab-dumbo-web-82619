1d20755166b8a7d157bf0399baa677910df452ee
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-23 15:30:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,8 @@ def find_the_cheese(array) #code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
-  if array.any? {|i| i = "cheddar"}
-    return "c"
+  if array.any? {|i| i = array}
+  
+  else
+    return nil
 end
