454cc879f26e9e5e1de1e51b2eb60e0e2e86e051
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-23 15:18:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,12 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
+  new = []
+  i = 0 
+  while i < array.length do
+    new << array[i].capitalize + "!"
+    i += 1 
+  end 
 end
 
 def long_planeteer_calls# code an argument here
